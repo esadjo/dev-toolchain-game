cca5624848504a219913274d225b06bc3e843dbf
Emma Sadjo <esadjo@uw.edu>
2023-02-10 19:13:56 -0800

    v 14

--- index.js
+++ index.js
@@ -9,23 +9,62 @@
 
 // https://p5play.org/learn/sprite.html?page=3
 let player;
+let balls;
+let character;
+let fires;
 
 window.setup = () => {
 	new Canvas(windowWidth, windowHeight);
 
 	player = new Sprite();
-	player.img = 'images/flower.png';
+	player.img = 'images/balloon.png';
 	player.diameter = 70;
+
+  // Reference -- https://editor.p5js.org/mbardin/sketches/OyZLpQW6N 
+
+    balls = new Group();
+    fires = new Group();
+  
+    for (let i = 0; i < 4; i++) {
+        character = new Sprite();
+        character.img = 'images/fire.png';
+        //character.rotation = 0;
+        //character.rotationSpeed = 1;
+        //character.setSpeed(random(12), random(360));
+        fires.add(character);
+
+       /* let c = new Sprite();
+        c.img = 'images/fire.png';
+        c.diameter = random(50, 100);
+        balls.add(c);*/
+    }
 };
 
 window.draw = () => {
 	//clear();
     // Using code from "Sprites on the move" of https://creative-coding.decontextualize.com/making-games-with-p5-play/
-    background(255);
+    background(139, 188, 234);
     textAlign(CENTER, CENTER);
     text("use arrow keys, or SPACE to stop",
     width/2, height*0.67);
     drawSprites();
+    
+      for (let i = 0; i < fires.length; i++) {
+        fires[i].position.x += fires[i].width * 0.01;
+        if (fires[i].position.x > windowWidth) {
+          fires[i].position.x = random(windowWidth - 100);
+        }
+        if (fires[i].position.y > windowHeight) {
+          fires[i].position.y = random(windowHeight - 100);
+        }
+       // fires[i].rotation = 0;
+        fires[i].rotationSpeed = 1;
+        fires[i].setSpeed(random(12), random(100));
+       // fires[i].setSpeed(random(12), random(360));
+        //fires[i].position.y = random(0, windowHeight); //Added
+      }
+    drawSprites();
+
     
     // Sprites on the move from https://creative-coding.decontextualize.com/making-games-with-p5-play/
     /*for (let i = 0; i < 10; i++) {
@@ -66,40 +105,4 @@ window.keyPressed = () => {
       player.setSpeed(0, 0);
     }
     return false;  
-  };
-
-
-/*
-let balls;
-*/
-/*
-window.setup = () => {
-    createCanvas(windowWidth, windowHeight);
-    balls = new Group();
-  
-    for (let i = 0; i < 10; i++) {
-        let c = new Sprite();
-        c.diameter = random(50, 100);
-*/
-      /*let c = createSprite(
-        random(width), random(height),
-        random(25, 100), random(25, 100));*/
- /*
-        c.shapeColor = color(random(200, 255));
-      balls.add(c);
-    }
-  };
-  
-  window.draw = () => {
-    background(0, 0, 0);
-  for (let i = 0; i < balls.length; i++) { // modified balls.length --> 
-    balls[i].position.x += balls[i].width * 0.01;
-    if (balls[i].position.x > width) {
-      balls[i].position.x = 0;
-    }
-    balls[i].position.y = random(0, windowHeight); //Added
-  }
-  drawSprites();
-  };
-
-*/
+};
