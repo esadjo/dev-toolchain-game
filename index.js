423be5a83a2919797765f05c9df7b488b7fb0973
Emma Sadjo <esadjo@uw.edu>
2023-02-18 15:00:40 -0800

    v 40

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-// To do: 
-//   - Make fire not just move vertically (make it more random and also SLOWER!!!)
-//   - Add levels of difficulty ?
-//   - Add button to restart game
-//   - 
-
 // Also reference -- https://p5play.org/learn/ 
 
 // https://creative-coding.decontextualize.com/making-games-with-p5-play/
 // Pulling from Sprite Groups example
-
-//sprite that move with keyboard 
-// create bullet --> that increases size 
-// work on core functionality
 
 // https://p5play.org/learn/sprite.html?page=3
 let player;
@@ -59,9 +49,9 @@ window.setup = () => {
   
    // while (fires.length < 2) {
       for (let i = 0; i < 5; i++) { // before was 1
-          fire = new Sprite(random(windowWidth), random(windowHeight)); // (random(windowWidth), random(windowHeight));
+          fire = new Sprite(random(windowWidth), random(windowHeight)); 
           fire.img = 'images/fire.png';
-          fire.speed = random(10); // See if can make it more random (so doesn't just go in a straight line) -- kind of did with random speed instead of speed of just 1
+          fire.speed = random(10); 
           fire.rotationSpeed  = random(10); //0.5;
           fire.position.x = random(windowWidth);
           fire.position.y = random(windowHeight);
@@ -72,27 +62,16 @@ window.setup = () => {
 };
 
 window.draw = () => {
-	//clear();
     // Using code from "Sprites on the move" of https://creative-coding.decontextualize.com/making-games-with-p5-play/
-    //background(1, 36, 68);
-    // PULLING FROM INTERACTIVE SKETCH
     background(backgroundImg);
 
-    //background(0, 0, 0);
-    //background(200*20*(score+1), 200*(36+score), 200*68*(score+1));
     for (let i = 0; i < 20; i++) {
-      //fill(i*5, i*20, i*40)
-      //fill(115 - (2*i), 191 - (8*i), 184 - i);
-      //fill(115 - (mouseX*30), 191 - (8*i), 184 - i);
       noStroke();
-      //stroke(100 - (mouseX/3), 91 - (8*i), 150 - 2*(mouseY/100));
       strokeWeight(1+ player.position.x/400);
       fill(80 + (3*score*i), 1+1.1*score, 1.1*(score+1));
-      //fill(20*(score+1), 36+score, 68*(score+1));
-      //fill(200 - (player.position.x/3), 191 - (8*i), 250 - 2*(player.position.x/100));
       bezier(5*score/2 - i*1000, -2000 - i*10, 100*(1+score)/2 + 2000, 0, 0, 0, -1000, windowHeight*2 + 20000);
-      //bezier(player.position.y + i*3000, 0 + i*10, player.position.x - 4000, player.position.y - 1000, 0, 400, 1000, windowHeight + 1000);
     }
+
     textAlign(CENTER, CENTER);
     fill(255);
 
@@ -201,12 +180,9 @@ window.mousePressed = () => {
     song.loop();
     musiccontrol.remove();
     musiccontrol = createSprite(windowWidth/8*7, windowHeight/8*1);
-    //if (musiccontrol.mouseIsOver) {
-      musiccontrol.addImage(musicImgPause);
-      musiccontrol.scale = 0.15;
-      musiccontrol.collider = 'none';
-    //}
-    
+    musiccontrol.addImage(musicImgPause);
+    musiccontrol.scale = 0.15;
+    musiccontrol.collider = 'none';
   }
 };
 
